b75b5b138abc1f2261f43984e8ab547e82d2de42
Charles Arciniega <charlesarcin@gmail.com>
2014-12-20 18:53:53 -0800

    Move printing behavior into a helper method and pass it a variable

--- baby-steps/Hello.java
+++ baby-steps/Hello.java
@@ -2,6 +2,11 @@ public class Hello {
   // When executing behavior within a class, you will always execute it in the `main` function
   // of that class.
   public static void main (String[] args) {
-    System.out.println("Hello World");
+    String name = "Charles";
+    hello(name);
+  }
+
+  public static void hello (String name) {
+    System.out.println(name);
   }
 }
