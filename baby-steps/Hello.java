c9fdd34cba6c00d97be711900749b520721abe41
Charles Arciniega <charlesarcin@gmail.com>
2014-12-20 18:42:05 -0800

    hello world function

--- baby-steps/Hello.java
+++ baby-steps/Hello.java
@@ -1,3 +1,7 @@
 public class Hello {
-  
+  // When executing behavior within a class, you will always execute it in the `main` function
+  // of that class.
+  public static void main (String[] args) {
+    System.out.println("Hello World");
+  }
 }
